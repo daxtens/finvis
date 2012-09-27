ad24cdc691345143a150f02af05d8120b7c13583
Daniel Axtens <dja@ucc.asn.au>
2012-09-27 20:36:22 +0800

    WA data

--- data.js
+++ data.js
@@ -70,3 +70,68 @@ var cth_fbo = {
 
 	'relations':{}
 };
+
+var wa_fbo = {
+	'name': 'WA Final Budget Outcome',
+
+	'aggregates': [
+		{
+			'name': 'Revenue',
+			'periods': {
+				'2011-12': {
+				  'value': 25.233*bil
+				},
+				'2010-11': {
+				  'value': 23.764*bil
+				}
+			},
+			'metadata': {
+				'cssClass': 'revenue',
+			}
+		},
+		{ 
+			'name': 'Expenses',
+			'periods': {
+				'2011-12': {
+				  'value': 24.791*bil
+				},
+				'2010-11': {
+				  'value': 22.98*bil
+				}
+			},
+			'metadata': {
+				'cssClass': 'expenses',
+			}
+		},
+		{
+			'name': 'Assets',
+			'periods': {
+				'2011-12': {
+				  'value': 135*bil
+				},
+				'2010-11': {
+				  'value': 129.116*bil
+				}
+			},
+			'metadata': {
+				'cssClass': 'assets',
+			}
+		},
+		{
+			'name': 'Liabilities',
+			'periods': {
+				'2011-12': {
+				  'value': 21.039*bil
+				},
+				'2010-11': {
+				  'value': 17.851*bil
+				}
+			},
+			'metadata': {
+				'cssClass': 'liabilities',
+			}
+		}
+	],
+
+	'relations':{}
+};
