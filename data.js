2132a5256056e46d0b24aa71b1b781122c424a9a
Daniel Axtens <daniel@axtens.net>
2012-12-05 11:41:34 +0800

    more data

--- data.js
+++ data.js
@@ -408,5 +408,474 @@ var usa = {
 	}
 };
 
-var entities = [ cth_fbo, wa_fbo, bhp, usa ];
+var abudhabi = {
+	'name': 'Abu Dhabi Investment Authority',
+	'aggregates': [
+		{
+			'name': 'Assets',
+			'periods': {
+				'2011-12': {
+					'value': 627*bil
+				}
+			},
+			'metadata': {
+				'cssClass': 'assets'
+			}
+		}
+	],
+	'relations': {}
+};
+
+var futurefund = {
+	'name': 'Future Fund',
+	'aggregates': [
+		{
+			'name': 'Assets',
+			'periods': {
+				'2011-12': {
+					'value': 73*bil
+				}
+			},
+			'metadata': {
+				'cssClass': 'assets'
+			}
+		}
+	],
+	'relations': {}
+};
+
+var submarines = {
+	'name': 'Submarines',
+	'aggregates': [
+		{
+			'name': 'Expense',
+			'periods': {
+				'2011-12': {
+					'value': 80*bil
+				}
+			},
+			'metadata': {
+				'cssClass': 'expenses'
+			}
+		}
+	],
+	'relations': {}
+};
+
+var australiansuper = {
+	'name': 'Australian Super Funds',
+	'aggregates': [
+		{
+			'name': 'Assets',
+			'periods': {
+				'2011-12': {
+					'value': 1277*bil
+				}
+			},
+			'metadata': {
+				'cssClass': 'revenue'
+			}
+		}
+	],
+	'relations': {}
+};
+
+
+
+var electioninitatives = {
+'name': 'United States Government Statements of Operations and Changes in Net Position',
+//lie about the period... FIXME
+	'aggregates': [
+		{
+			'name': 'Receipts',
+			'periods': {
+				'2011-12': {
+					'value': 2363.8*bil
+				}
+			},
+			'metadata': {
+				'cssClass': 'revenue'
+			}
+		},
+		{
+			'name': 'Outlays',
+			'periods': {
+				'2011-12': {
+					'value': 3660.8*bil
+				}
+			},
+			'metadata': {
+				'cssClass': 'expenses'
+			}
+		},
+		{
+			'name': 'Assets',
+			'periods': {
+				'2011-12': {
+					'value': 2707.3*bil
+				}
+			},
+			'metadata': {
+				'cssClass': 'assets'
+			}
+		},
+		{
+			'name': 'Liabilities',
+			'periods': {
+				'2011-12': {
+					'value': 17492.7*bil
+				}
+			},
+			'metadata': {
+				'cssClass': 'liabilities'
+			}
+		}
+	],
+	'relations': {
+		'revenueVexpenses': {
+		  'greater': 'Budget Surplus',
+		  'equal': 'Balanced Budget',
+		  'less': 'Budget Deficit'
+		}
+	}
+};
+
+var greens_budget = {
+    'name': 'Greens Budget Initatives',
+    'aggregates': [
+	{ 'name': 'Revenue',
+	  'periods': {
+	      '2011-12': {// this is a lie
+		  'value':  25487*mil,//125*bil,
+		  'items': [ 
+		      { 'name':'Revised MRRT',
+			'value': 6000*mil
+		      },
+		      { 'name':'Tax measure 1',
+			'value': 235*mil
+		      },
+		      { 'name':'Tax measure 2',
+			'value': 2600*mil
+		      },
+		      { 'name':'Tax measure 3',
+			'value': 2500*mil
+		      },
+		      { 'name':'Tax measure 4',
+			'value': 600*mil
+		      },
+		      { 'name':'Tax measure 5',
+			'value': 200*mil
+		      },
+		      { 'name':'Tax measure 6',
+			'value': 2500*mil
+		      },
+		      { 'name':'Tax measure 7',
+			'value': 900*mil
+		      },
+		      { 'name':'Tax measure 8',
+			'value': 1250*mil
+		      },
+		      { 'name':'Tax measure 9',
+			'value': 150*mil
+		      },
+		      { 'name':'Tax measure 10',
+			'value': 1170*mil
+		      },
+		      { 'name':'Tax measure 11',
+			'value': 1000*mil
+		      },
+		      { 'name':'Tax measure 12',
+			'value': 172*mil
+		      },
+		      { 'name':'Tax measure 13',
+			'value': 260*mil
+		      },
+		      { 'name':'Tax measure 14',
+			'value': 100*mil
+		      },
+		      { 'name':'Tax measure 15',
+			'value': 4500*mil
+		      },
+		      { 'name':'Tax measure 16',
+			'value': 1200*mil
+		      },
+		      { 'name':'Tax measure 17',
+			'value': 150*mil
+		      }
+		  ]
+	      }
+	  },
+	  'metadata': {
+	      'cssClass': 'revenue'
+	  }
+	},
+	{ 'name': 'Expenses',
+
+	  'periods': {
+	      '2011-12': { //lie
+
+		  'value': 23943.3*mil,//117*bil,//23943.3
+		  'items': [
+		      { 'name':'Environment',
+			'value': 380*mil,
+			'items': [
+			    { 'name':'Environment expenditure 1',
+			      'value': 95*mil
+			    },
+			    { 'name':'Environment expenditure 2',
+			      'value': 30*mil
+			    },
+			    { 'name':'Environment expenditure 3',
+			      'value': 71*mil
+			    },
+			    { 'name':'Environment expenditure 4',
+			      'value': 58*mil
+			    },
+			    { 'name':'Environment expenditure 5',
+			      'value': 10*mil
+			    },
+			    { 'name':'Environment expenditure 6',
+			      'value': 50*mil
+			    },
+			    { 'name':'Environment expenditure 7',
+			      'value': 26*mil
+			    },
+			    { 'name':'Environment expenditure 8',
+			      'value': 40*mil
+			    }]
+		      },
+		      
+		      {'name': 'Climate change and energy',
+		       'value': 77.3*mil,
+		       'items': [
+			   { 'name':'Climate expenditure 1',
+			     'value': 0*mil
+			   },
+			   { 'name':'Climate expenditure 2',
+			     'value': 0*mil
+			   },
+			   { 'name':'Climate expenditure 3	-1',
+			     'value': 7*mil
+			   },
+			   { 'name':'Climate expenditure 4',
+			     'value': -1*mil
+			   },
+			   { 'name':'Climate 1',
+			     'value': 	20*mil
+			   },
+			   { 'name':'Climate 2',
+			     'value': 60*mil
+			   }]
+		      },
+		      
+		      
+		      
+		      {'name': 'Education science and industry',
+		       'value': 2135*mil,
+		       'items': [
+			   { 'name':'Education expenditure 1',
+			     'value': 700*mil
+			   },
+			   { 'name':'Education expenditure 2',
+			     'value': 300*mil
+			   },
+			   { 'name':'Education expenditure 3',
+			     'value': 190*mil
+			   },
+			   { 'name':'Education expenditure 4',
+			     'value': 350*mil
+			   },
+			   { 'name':'Education expenditure 5',
+			     'value': 35*mil
+			   },
+			   { 'name':'Education expenditure 6',
+			     'value': 20*mil
+			   },
+			   { 'name':'Education expenditure 7',
+			     'value': 300*mil
+			   },
+			   { 'name':'Education expenditure 8',
+			     'value': 38*mil
+			   },
+			   { 'name':'Education expenditure 9',
+			     'value': 33*mil
+			   },
+			   { 'name':'Education expenditure 10',
+			     'value': 169*mil
+			   }]
+		      },
+		      
+		      
+		      {'name': 'Care for people',
+		       'value': 4488*mil,
+		       'items': [
+			   { 'name':'Denticare',
+			     'value': 656*mil
+			   },
+			   { 'name':'People expenditure 1',
+			     'value': 1200*mil
+			   },
+			   { 'name':'People expenditure 2',
+			     'value': 1458*mil
+			   },
+			   { 'name':'People expenditure 3',
+			     'value': 400*mil
+			   },
+			   { 'name':'People expenditure 4',
+			     'value': 180*mil
+			   },
+			   { 'name':'People expenditure 5',
+			     'value': 160*mil
+			   },
+			   { 'name':'People expenditure 6',
+			     'value': 330*mil
+			   },
+			   { 'name':'People expenditure 7',
+			     'value': 20*mil
+			   },
+			   { 'name':'People expenditure 8',
+			     'value': 84*mil
+			   }]
+		      },
+		      
+		      
+		      {'name': 'Housing and sustainable cities',
+		       'value': 8960*mil,
+		       'items': [
+			   { 'name':'Housing expenditure 1',
+			     'value': 5400*mil
+			   },
+			   { 'name':'Housing expenditure 2',
+			     'value': 1300*mil
+			   },
+			   { 'name':'Housing expenditure 3',
+			     'value': 500*mil
+			   },
+			   { 'name':'Housing expenditure 4',
+			     'value': 300*mil
+			   },
+			   { 'name':'Housing expenditure 5',
+			     'value': 1000*mil
+			   },
+			   { 'name':'Housing expenditure 6',
+			     'value': 210*mil
+			   },
+			   { 'name':'Housing expenditure 7',
+			     'value': 250*mil
+			   }]
+		      },
+		      
+		      
+		      {'name': 'Transport',
+		       'value': 2166*mil,
+		       'items': [
+			   { 'name':'Bike paths national',
+			     'value': 80*mil
+			   },
+			   { 'name':'Transport expenditure 1',
+			     'value': 275*mil
+			   },
+			   { 'name':'Transport expenditure 2',
+			     'value': 250*mil
+			   },
+			   { 'name':'Transport expenditure 3',
+			     'value': 30*mil
+			   },
+			   { 'name':'Transport expenditure 4',
+			     'value': 300*mil
+			   },
+			   { 'name':'Transport expenditure 5',
+			     'value': 12*mil
+			   },
+			   { 'name':'Transport expenditure 6',
+			     'value': 100*mil
+			   },
+			   { 'name':'Transport expenditure 7',
+			     'value': 144*mil
+			   },
+			   { 'name':'Transport expenditure 8',
+			     'value': 444*mil
+			   },
+			   { 'name':'Transport expenditure 9',
+			     'value': 531*mil
+			   }]
+		      },
+		      
+		      
+		      {'name': 'Other',
+		       'value': 5737*mil,
+		       'items': [
+			   { 'name':'Foreign aid',
+			     'value': 2000*mil
+			   },
+			   { 'name':'Other 1',
+			     'value': 1000*mil
+			   },
+			   { 'name':'Other 2',
+			     'value': 7*mil
+			   },
+			   { 'name':'Other 3',
+			     'value': 90*mil
+			   },
+			   { 'name':'Other 4',
+			     'value': 0*mil
+			   },
+			   { 'name':'Other 5',
+			     'value': 0*mil
+			   },
+			   { 'name':'Other 6',
+			     'value': 20*mil
+			   },
+			   { 'name':'Other 7',
+			     'value': 29*mil
+			   },
+			   { 'name':'Other 8',
+			     'value': 5*mil
+			   },
+			   { 'name':'Other 9',
+			     'value': 12*mil
+			   },
+			   { 'name':'Other 10',
+			     'value': 124*mil
+			   },
+			   { 'name':'Other 11',
+			     'value': 12*mil
+			   },
+			   { 'name':'Other 12',
+			     'value': 5*mil
+			   },
+			   { 'name':'Other 13',
+			     'value': 34*mil
+			   },
+			   { 'name':'Other 14',
+			     'value': 2354*mil
+			   },
+			   { 'name':'Other 15',
+			     'value': 12*mil
+			   },
+			   { 'name':'Other 16',
+			     'value': 33*mil
+			   }
+		       ]
+		      }
+		  ]
+	      }
+	  },
+
+	  'metadata': {
+	      'cssClass': 'expenses'
+	  }
+	}	
+    ],
+    'relations': {
+	'revenueVexpenses': {
+	    'greater': 'Budget Surplus',
+	    'equal': 'Balanced Budget',
+	    'less': 'Budget Deficit'
+	}
+    }
+}
+
+
+var entities = [ cth_fbo, wa_fbo, bhp, usa, abudhabi, futurefund, submarines, greens_budget ];
 
