25d2c3580159049f7b6e36c884d9ea3f21866cf5
Daniel Axtens <daniel@axtens.net>
2013-01-06 02:28:17 +0800

    clean events.js

--- events.js
+++ events.js
@@ -3,69 +3,69 @@
 /* Event dispatcher. */
 
 /* Keys */
-document.onkeydown = function (e) {
-	// ... is this necessary in the browsers we're supporting?
-	e = e || window.event;
-	
-	var left_arrow_key = 37;
-	var up_arrow_key = 38;
-	var right_arrow_key = 39;
-	var down_arrow_key = 40;
-		
-	var delta = 20;
+document.onkeydown = function(e) {
+    // ... is this necessary in the browsers we're supporting?
+    e = e || window.event;
 
-	var position = viewstate.position;
-	var move = true;
+    var left_arrow_key = 37;
+    var up_arrow_key = 38;
+    var right_arrow_key = 39;
+    var down_arrow_key = 40;
 
-	if (e.keyCode == left_arrow_key) {
-		position[0] -= delta;
-	} else if (e.keyCode == up_arrow_key) {
-		position[1] -= delta;
-	} else if (e.keyCode == right_arrow_key) {
-		position[0] += delta;
-	} else if (e.keyCode == down_arrow_key) {
-		position[1] += delta;
-	} else {
-		move = false;
-	}
-	if (move) viewstate.moveTo( position );
-}
+    var delta = 20;
+
+    var position = viewstate.position;
+    var move = true;
+
+    if (e.keyCode == left_arrow_key) {
+        position[0] -= delta;
+    } else if (e.keyCode == up_arrow_key) {
+        position[1] -= delta;
+    } else if (e.keyCode == right_arrow_key) {
+        position[0] += delta;
+    } else if (e.keyCode == down_arrow_key) {
+        position[1] += delta;
+    } else {
+        move = false;
+    }
+    if (move) viewstate.moveTo(position);
+};
 
 
 /* Resize */
 var onResizeTimer;
-window.onresize = function () {
-    try { 
-	window.clearTimeout(onResizeTimer); 
+window.onresize = function() {
+    try {
+        window.clearTimeout(onResizeTimer);
     } catch (err) {}
-    
-    onResizeTimer = window.setTimeout( function() {
-	viewstate.calculateSize(tril);
-	viewstate.children().map( function(child) { child.render() } );
-    }, 50 );
-}
+
+    onResizeTimer = window.setTimeout(function() {
+        viewstate.calculateSize(tril);
+        viewstate.children().map(function(child) { child.render() });
+    }, 50);
+};
 
 /** Initalise the document when we start. */
 jQuery('document').ready(function() {
     var entitySel = jQuery('#entitySel');
     jQuery.each(entities, function(index) {
         entitySel.append(
-            jQuery('<option />').val( index ).text( entities[index].name )
+            jQuery('<option />').val(index).text(entities[index].name)
         );
     });
-		
-    window.viewstate = new ViewState(d3.select("body").append("svg"));
 
-    var vo = new ViewObj( openbudget, viewstate, [0,0] );
+    window.viewstate = new ViewState(d3.select('body').append('svg'));
+
+    var vo = new ViewObj(openbudget, viewstate, [0, 0]);
     vo.period('2011-12');
     vo.render();
 });
 
 /** Handler for the period selector
-    @param sel The <select> tag.
+    @param {Object} sel The <select> tag.
 */
 window['periodChange'] = function(sel) {
-    var chosenoption=sel.options[sel.selectedIndex];
+    var chosenoption = sel.options[sel.selectedIndex];
     viewstate.children().map(function(obj) {obj.period(chosenoption.value);});
     viewstate.children().map(function(obj) {obj.render();});
 };
@@ -73,5 +73,5 @@ window['periodChange'] = function(sel) {
 /** Handler for add entity button */
 window['addEntity'] = function() {
     var entitySel = jQuery('#entitySel')[0];
-    viewstate.beginAddingView( entities[entitySel.selectedIndex] );
+    viewstate.beginAddingView(entities[entitySel.selectedIndex]);
 };
