472ecf4f1d985a05758c13ca7ecabbee99c42524
Bruce M. Axtens <bruce.axtens@gmail.com>
2013-01-27 16:54:57 +0800

    Improved formatting of jsdoc markup (Task #1)

--- parenting.js
+++ parenting.js
@@ -1,30 +1,30 @@
 
 "use strict";
 /** A framework for parent/child objects 
-	@constructor
-*/
+ *	@constructor
+ */
 function ParentingObject() {
 	
 	/** create array for storage of objects */ 	
 	var _children = [];
 
 	/** return array of Objects
-	@returns {Array} Array of objects
-	*/ 	
+	 * @returns {Array} Array of objects
+	 */ 	
 	this.children = function () {
 		return _children;
 	}
 
 	/** add child to array of Objects 
-	@param {Object} child A ViewState or ViewObj object
-	*/ 	
+	 * @param {Object} child A ViewState or ViewObj object
+	 */ 	
 	this.addChild = function (child) {
 		_children.push(child);
 	}
 
 	/** remove child from array of Objects
-	@param {Object} child A ViewState or ViewObj object
-	*/ 	
+	 * @param {Object} child A ViewState or ViewObj object
+	 */ 	
 	this.removeChild = function (child) {
 		_children.splice(_children.indexOf(child),1);
 	}
