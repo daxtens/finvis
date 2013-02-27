3e1d8daac0c616bf79aa481571e37ee65f0533f8
Daniel Axtens <daniel@axtens.net>
2013-02-27 22:55:54 +1100

    Make fabfile more robust

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,15 @@
 from fabric.api import *
 
+srcfiles = ['circles.js', 'data.js', 'theopenbudget.js', 'parenting.js',
+            'viewstate.js', 'viewobj.js', 'events.js']
+
+
 def hello():
         print( "Hello world!" )
 
 def compile(advanced="False"):
-        files = ['d3.v3.js', 'jquery-1.8.3.js', 'jquery.contextmenu.r2.js',
-                 'circles.js', 'data.js', 'theopenbudget.js', 'parenting.js',
-                 'viewstate.js', 'viewobj.js', 'events.js']
+        files = ['d3.v3.js', 'jquery-1.8.3.js', 'jquery.contextmenu.r2.js'] + \
+            srcfiles
 
         command = "java -jar ../compiler-latest/compiler.jar "
         command = command + "--language_in=ECMASCRIPT5_STRICT "
@@ -23,4 +26,5 @@ def compile(advanced="False"):
         local( command )
 
 def docs():
-        local( "../jsdoc/jsdoc . --destination ./docs/" )
+        local( "../jsdoc/jsdoc " + \
+               " ".join(srcfiles) + " --destination ./docs/" )
