3ad28145a6915b639fbfe506742c0e0d6c352a3c
Bruce M. Axtens <bruce.axtens@gmail.com>
2013-04-10 18:12:48 +0800

    line length changes and tabs

--- parenting.js
+++ parenting.js
@@ -1,33 +1,36 @@
 
-"use strict";
-/** A framework for parent/child objects 
- *	@constructor
+'use strict';
+
+
+
+/** A framework for parent/child objects
+ *  @constructor
  */
 function ParentingObject() {
-	
-	/** create array for storage of objects
-            @private */ 	
-	var _children = [];
 
-	/** return my children: array of ParentingObjects
-	 * @returns {Array.<ParentingObject>} my children
-	 */ 	
-	this.children = function () {
-		return _children;
-	}
+  /** create array for storage of objects
+      @private */
+  var _children = [];
 
-	/** add child
-	 * @param {ParentingObject} child A ParentingObject (ViewState or ViewObj)
-	 */ 	
-	this.addChild = function (child) {
-		_children.push(child);
-	}
+  /** return my children: array of ParentingObjects
+   * @return {Array.<ParentingObject>} my children.
+   */
+  this.children = function() {
+    return _children;
+  };
 
-	/** remove child
-	 * @param {ParentingObject} child A ParentingObject (ViewState or ViewObj)
-	 */ 	
-	this.removeChild = function (child) {
-		_children.splice(_children.indexOf(child),1);
-	}
+  /** add child
+   * @param {ParentingObject} child A ParentingObject (ViewState or ViewObj).
+   */
+  this.addChild = function(child) {
+    _children.push(child);
+  };
+
+  /** remove child
+   * @param {ParentingObject} child A ParentingObject (ViewState or ViewObj).
+   */
+  this.removeChild = function(child) {
+    _children.splice(_children.indexOf(child), 1);
+  };
 
 }
