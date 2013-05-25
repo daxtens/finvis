01f9891e926b96bc3e8a18faebf7727a625e4962
Daniel Axtens <daniel@axtens.net>
2013-05-25 17:26:10 +1000

    Update to fabfile to help deploy on other servers.

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 from fabric.api import *
 
-srcfiles = ['circles.js', 'data.js', 'theopenbudget.js', 'parenting.js',
+srcfiles = ['circles.js', 'util.js', 'parenting.js',
             'viewstate.js', 'viewobj.js', 'events.js']
 
 
@@ -9,27 +9,26 @@ def hello():
 
 def setup(dev="False"):
         # web framework
-        local("pip install bottle")
-        local("pip install CherryPy")  # (accelerated server)
+        run("pip install bottle")
+        run("pip install CherryPy")  # (accelerated server)
         # auth
-        local("pip install bottle-cork")
+        run("pip install bottle-cork")
         # excel
-        local("pip install xlrd xlwt")
+        run("pip install xlrd xlwt")
         # database
-        local("pip install pymongo mongoengine")
+        run("pip install pymongo mongoengine")
         # PNG export: verify inkscape is installed
-        local("inkscape --version")
+        run("inkscape --version")
 
 	if dev == "True":
-		local("pip install pep8")
-		local("pip install http://sourceforge.net/projects/pychecker/files/pychecker/0.8.19/pychecker-0.8.19.tar.gz/download")
-		local("pip install nose")
-		local("easy_install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz")
+		run("pip install pep8")
+		run("pip install http://sourceforge.net/projects/pychecker/files/pychecker/0.8.19/pychecker-0.8.19.tar.gz/download")
+		run("pip install nose")
+		run("easy_install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz")
 
 
-def compile(advanced="False"):
-        files = ['d3.v3.js', 'jquery-1.8.3.js', 'jquery.contextmenu.r2.js'] + \
-            srcfiles
+def compile(advanced="False", pretty="True"):
+        files = srcfiles
 
         command = "java -jar ../compiler-latest/compiler.jar "
         command = command + "--language_in=ECMASCRIPT5_STRICT "
@@ -40,7 +39,8 @@ def compile(advanced="False"):
                 command = (command +
                            "--compilation_level ADVANCED_OPTIMIZATIONS ")
 
-        command = command + "--formatting=pretty_print "
+	if pretty == "True":
+                command = command + "--formatting=pretty_print "
         
         command = command + "--js_output_file=finvis.js"
         local( command )
@@ -49,3 +49,18 @@ def docs():
         local( "../jsdoc/jsdoc " + \
                " ".join(srcfiles) + " --destination ./docs/" )
 
+def init_machine():
+        # do system updates
+        run("sudo apt-get update")
+        run("sudo apt-get -y upgrade")
+
+        #install mongodb
+        run("sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10")
+        run("echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list")
+        run("sudo apt-get update")
+        run("sudo apt-get -y install mongodb-10gen")
+
+        # install various dependencies
+        run("sudo apt-get install -y nginx inkscape fabric python-virtualenv supervisor git")
+
+        print("You now have the required packages.")
